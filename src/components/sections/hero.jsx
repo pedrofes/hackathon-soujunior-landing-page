09ebf2c72eb2a9485dbcf93729c0ffa69600076f
duanny.evelyn <99351100+du-almeida@users.noreply.github.com>
2026-09-20 11:40:19 -0300

    fix: ajuste dimencionamento dos elementos, deixando exatamente as medidas do figma, apenas o mascote esta proporcional

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -7,28 +7,31 @@ export default function Hero() {
     const [isPressed, setIsPressed] = useState(false);
 
     return (
-        <section id="hero" className="w-full min-h-[calc(100vh+100px)] -mt-[100px] pt-[100px] flex items-center px-6 border-b border-neutral/10 bg-[#0E14BF]">
+        <section id="hero" className="w-full min-h-screen -mt-[79px] pt-[79px] flex items-center px-6 border-b border-neutral/10 bg-[#0E14BF]">
             <div className="max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-center gap-10 md:gap-16">
                     <div className="text-left">
                         <div className="text-left">
-                        <h1 className="font-display text-[52px] font-extrabold text-[#E7E8EA] mt-2 mb-4 text-left">
+                        <h1 className="font-display text-[52px] font-extrabold text-[#E7E8EA] mt-2 mb-6 text-left">
                             Quem está começando <br />
                             hoje pode <span className="text-yellow-accent">transformar</span> <br />
                             a tecnologia amanhã.
                         </h1>
-                        <h3 className="font-sans text-[#E7E8EA] max-w-xl mb-6 text-left">
+                        <h3 className="font-sans text-[#E7E8EA] max-w-xl mb-8 text-left text-[24px]">
                             Apoie profissionais juniores e ajude a fortalecer uma<br />
                             comunidade que transforma início de carreira em<br /> oportunidade.
                         </h3>
                     </div>
-                    <div className="text-left mt-8">
+                    <div className="text-left mt-10">
                         <button
-                            onClick={() => setIsPressed(true)}
-                            className={`bg-[#0A1662] text-white font-bold py-3 px-6 rounded-lg hover:bg-[#171123E5] active:bg-[#3C7EF9] transition-transform duration-200 hover:scale-105 ${isPressed ? "scale-105" : ""}`}
+                            onClick={() => {
+                                setIsPressed(true);
+                                document.getElementById("planos")?.scrollIntoView({ behavior: "smooth" });
+                            }}
+                            className={`bg-[#171123] text-[24px] text-white font-bold py-3 px-6 w-[300px] h-[72px] rounded-2xl hover:bg-[#171123E5] active:bg-[#3C7EF9] transition-transform duration-200 hover:scale-105 ${isPressed ? "scale-105" : ""}`}
                         >
                             Apoie a SouJunior
                         </button>
-                        <p className="text-[#E7E8EA] mt-4">
+                        <p className="text-[#E7E8EA] mt-6 text-[16.89px]">
                             Apoie a partir de R$2/mês.
                         </p>
                     </div>
@@ -37,10 +40,10 @@ export default function Hero() {
                     <Image
                         src="/sou_junior_mascote.svg"
                         alt="Mascote da SouJunior"
-                        width={500}
-                        height={500}
+                        width={600}
+                        height={600}
                         priority
-                        className="w-full max-w-[420px] h-auto"
+                        className="w-full max-w-[500px] h-auto"
                     />
                 </div>
             </div>
